43aa630f5ceb50fb3a2fe7285b39f105a1b8a331
Yang <khucnam@yahoo.com>
2020-03-27 18:24:13 +0800

    Third commit

--- Predict.py
+++ Predict.py
@@ -216,29 +216,29 @@ for proteinID in fasttext_input_sequence_dic1.keys():
 f.close()
 
 
-# #SubSK
-# f=open("SubSK."+outputFile,"w")
-# f.write("ProteinID,Probability\n")
-# answerDict={}
-# for proteinID in fasttext_input_sequence_dic1.keys():
-    # f.write(proteinID+",")
-    # fasttext_input_sequence1=fasttext_input_sequence_dic1.get(proteinID)
-    # fasttext_input_sequence2=fasttext_input_sequence_dic2.get(proteinID)
-    # for cla in ["A"]:
-            # svm_input_file="tmp\\"+cla+"_"+proteinID+".csv"
-            # embedding_file1="fastText embedding vectors\\fastText embedding vectors\\SubSK\\ngram1.dfSubword.embedding.train.A.vec"
-            # embedding_file2="fastText embedding vectors\\fastText embedding vectors\\SubSK\\ngram1.dfSubword.embedding.train.A.vec"
-            # create_svm_input_from_one_seq(embedding_file1, embedding_file2, fasttext_input_sequence1, fasttext_input_sequence2, svm_input_file)
-            # model_file="Models\\Models\\SubSK\\"+cla+".pickle_model.pkl"
-            # answerForOneClass = run(svm_input_file,model_file)
-            # f.write(str("{:.3f}".format(answerForOneClass))+",")
-    # f.write("\n")
-# f.close()
+# SubSK
+f=open("SubSK."+outputFile,"w")
+f.write("ProteinID,Probability\n")
+answerDict={}
+for proteinID in fasttext_input_sequence_dic1.keys():
+    f.write(proteinID+",")
+    fasttext_input_sequence1=fasttext_input_sequence_dic1.get(proteinID)
+    fasttext_input_sequence2=fasttext_input_sequence_dic2.get(proteinID)
+    for cla in ["A"]:
+            svm_input_file="tmp\\"+cla+"_"+proteinID+".csv"
+            embedding_file1="fastText embedding vectors\\fastText embedding vectors\\SubSK\\ngram1.dfSubword.embedding.train.A.vec"
+            embedding_file2="fastText embedding vectors\\fastText embedding vectors\\SubSK\\ngram1.dfSubword.embedding.train.A.vec"
+            create_svm_input_from_one_seq(embedding_file1, embedding_file2, fasttext_input_sequence1, fasttext_input_sequence2, svm_input_file)
+            model_file="Models\\Models\\SubSK\\"+cla+".pickle_model.pkl"
+            answerForOneClass = run(svm_input_file,model_file)
+            f.write(str("{:.3f}".format(answerForOneClass))+",")
+    f.write("\n")
+f.close()
 
 
 #delete temporary files    
-# for filename in os.listdir("tmp"):
-    # os.remove("tmp\\"+filename)
+for filename in os.listdir("tmp"):
+    os.remove("tmp\\"+filename)
     
 
 print("Thank you for using FastET!!! Please check the prediction results in Result.csv file")
